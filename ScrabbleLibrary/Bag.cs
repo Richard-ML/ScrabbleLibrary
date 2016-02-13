a60a5da54ad85ba76f65596a1f3b3face1884fc1
Richard-ML <Richard.ML@Ubernetic.ca>
2016-02-13 17:54:31 -0500

    Added List letters and Dic LetterValues

--- ScrabbleLibrary/Bag.cs
+++ ScrabbleLibrary/Bag.cs
@@ -17,11 +17,56 @@ namespace ScrabbleLibrary
 {
     class Bag : IBag
     {
-        Bag() { }
+
+        private Dictionary<char, int> letterValue = new Dictionary<char, int>() { { 'a', 1 }, { 'b', 3 }, { 'c', 3 }, { 'd', 2 }, { 'e', 1 }, { 'f', 4 }, { 'g', 2 }, { 'h', 4 }, { 'i', 1 }, { 'j', 8 }, { 'k', 5 }, { 'l', 1 }, { 'm', 3 }, { 'n', 1 }, { 'o', 1 }, { 'p', 3 }, { 'q', 10 }, { 'r', 1 }, { 's', 1 }, { 't', 1 }, { 'u', 1 }, { 'v', 4 }, { 'w', 4 }, { 'x', 8 }, { 'y', 4 }, { 'z', 10 } };
+        private List<char> letters;
+
+        Bag() {
+            letters = new List<char>
+            {
+                'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A',
+                'B', 'B',
+                'C', 'C',
+                'D', 'D', 'D', 'D',
+                'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E',
+                'F', 'F',
+                'G', 'G', 'G',
+                'H', 'H',
+                'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I',
+                'J',
+                'K',
+                'L', 'L', 'L', 'L',
+                'M', 'M',
+                'N', 'N', 'N', 'N', 'N', 'N',
+                'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+                'P', 'P',
+                'Q',
+                'R', 'R', 'R', 'R', 'R', 'R',
+                'S', 'S', 'S', 'S',
+                'T', 'T', 'T', 'T', 'T', 'T',
+                'U', 'U', 'U', 'U',
+                'V', 'V',
+                'W', 'W',
+                'X',
+                'Y', 'Y',
+                'Z'
+            };
+
+            //shuffle letters
+        }
 
         public int GetPoints(string candidate)
         {
-            throw new NotImplementedException();
+            int score = 0;
+            candidate = candidate.ToLower();
+            int value;
+            
+            for (int nc = 0; nc < candidate.Length; nc++)
+            {
+                if (letterValue.TryGetValue(candidate.ElementAt(nc), out value))
+                     score += value;                
+            }
+            return score;
         }
 
         public string PlayWord(string candidate)
