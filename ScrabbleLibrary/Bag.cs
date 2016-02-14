81c70adc9b26929c8d47d5dcd36c7856e74a5694
Richard-ML <Richard.ML@Ubernetic.ca>
2016-02-13 20:49:04 -0500

    Added rack

--- ScrabbleLibrary/Bag.cs
+++ ScrabbleLibrary/Bag.cs
@@ -31,6 +31,19 @@ namespace ScrabbleLibrary
         private Dictionary<char, int> letterValue = new Dictionary<char, int>() { { 'a', 1 }, { 'b', 3 }, { 'c', 3 }, { 'd', 2 }, { 'e', 1 }, { 'f', 4 }, { 'g', 2 }, { 'h', 4 }, { 'i', 1 }, { 'j', 8 }, { 'k', 5 }, { 'l', 1 }, { 'm', 3 }, { 'n', 1 }, { 'o', 1 }, { 'p', 3 }, { 'q', 10 }, { 'r', 1 }, { 's', 1 }, { 't', 1 }, { 'u', 1 }, { 'v', 4 }, { 'w', 4 }, { 'x', 8 }, { 'y', 4 }, { 'z', 10 } };
         private List<char> letters;
 
+        public string rack
+        {
+            get
+            {
+                return rack;
+            }
+
+            set
+            {
+                rack = value;
+            }
+        }
+
         Bag() {
            List<char> temp = new List<char>
             {
