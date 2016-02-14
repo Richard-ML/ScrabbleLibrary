c8e59773d5ddddd9278264d94953564136ffddd1
triflin <craig.macleod1986@gmail.com>
2016-02-14 01:55:48 -0500

    ASDGA

--- ScrabbleLibrary/Bag.cs
+++ ScrabbleLibrary/Bag.cs
@@ -28,7 +28,7 @@ namespace ScrabbleLibrary
     public class Bag : IBag
     {
         private Dictionary<char, int> letterValue = new Dictionary<char, int>() { { 'a', 1 }, { 'b', 3 }, { 'c', 3 }, { 'd', 2 }, { 'e', 1 }, { 'f', 4 }, { 'g', 2 }, { 'h', 4 }, { 'i', 1 }, { 'j', 8 }, { 'k', 5 }, { 'l', 1 }, { 'm', 3 }, { 'n', 1 }, { 'o', 1 }, { 'p', 3 }, { 'q', 10 }, { 'r', 1 }, { 's', 1 }, { 't', 1 }, { 'u', 1 }, { 'v', 4 }, { 'w', 4 }, { 'x', 8 }, { 'y', 4 }, { 'z', 10 } };
-        private List<char> letters;
+        private List<char> letters = new List<char>();
         public int currPlayer = 1;
         public int numPlayers = 1;
 
