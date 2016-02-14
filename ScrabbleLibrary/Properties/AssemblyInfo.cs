ad98354fb3d9062887e962d737b940a22cc6220b
triflin <craig.macleod1986@gmail.com>
2016-02-14 01:00:35 -0500

    stuff

--- ScrabbleLibrary/Properties/AssemblyInfo.cs
+++ ScrabbleLibrary/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a8ed3178-b827-459b-8823-2bb4441c11b3")]
